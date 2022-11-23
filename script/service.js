2e30c4dc24bd3a68ded62b98681ab27a5e3e1096
Sergey Pokrovsky <sapokrovsky@gmail.com>
2022-11-24 00:12:14 +0300

    :)

--- script/service.js
+++ script/service.js
@@ -1,5 +1,16 @@
 const items = document.querySelectorAll('.services__item');
 
+const btnTreatment = document.getElementById('btnTreatment');
+const btnOrthopedics = document.getElementById('btnOrthopedics');
+const btnHygiene = document.getElementById('btnHygiene');
+const btnWhitening = document.getElementById('btnWhitening');
+
+const treatment = document.getElementById('treatment');
+const orthopedics = document.getElementById('orthopedics');
+const hygiene = document.getElementById('hygiene');
+const whitening = document.getElementById('whitening')
+
+
 items.forEach(i => {
     i.addEventListener('click', e => {
         addActiveLink(e);
@@ -15,3 +26,35 @@ const addActiveLink = (activelink) => {
         }
     })
 }
+
+btnTreatment.addEventListener('click',e =>{
+    treatment.classList.add('services__block-two_active');
+
+    orthopedics.classList.remove('services__block-two_active')
+    hygiene.classList.remove('services__block-two_active')
+    whitening.classList.remove('services__block-two_active')
+})
+
+btnOrthopedics.addEventListener('click',e =>{
+    orthopedics.classList.add('services__block-two_active');
+
+    treatment.classList.remove('services__block-two_active')
+    hygiene.classList.remove('services__block-two_active')
+    whitening.classList.remove('services__block-two_active')
+})
+
+btnHygiene.addEventListener('click',e =>{
+    hygiene.classList.add('services__block-two_active');
+
+    treatment.classList.remove('services__block-two_active')
+    orthopedics.classList.remove('services__block-two_active')
+    whitening.classList.remove('services__block-two_active')
+})
+
+btnWhitening.addEventListener('click',e =>{
+    whitening.classList.add('services__block-two_active');
+    
+    treatment.classList.remove('services__block-two_active')
+    hygiene.classList.remove('services__block-two_active')
+    orthopedics.classList.remove('services__block-two_active')
+})
