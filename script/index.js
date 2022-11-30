36377a46def29c8b56da1b901b3168681a66ef00
Sergey Pokrovsky <sapokrovsky@gmail.com>
2022-12-01 01:59:18 +0300

    feat: menu burger and alt for img

--- script/index.js
+++ script/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $('.menu__burger').click(function(event){
+        $('.menu__burger,.menu__links,.span,.menu__entry,.menu__line').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+$(document).ready(function(){
     $('.slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
